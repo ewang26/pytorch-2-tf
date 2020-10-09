805ae358f7d745020d3b7a1394f108709ad0ff48
iRove108 <iRove108@users.noreply.github.com>
2020-10-08 22:36:49 -0500

    Add more pretrained models to the get_model_arch function

--- models/models.py
+++ models/models.py
@@ -3,6 +3,27 @@ import torchvision.models as models
 def get_model_arch(model_str):
     if model_str == 'resnet18':
         return models.resnet18(pretrained=True)
-    #TODO add more models
+    elif model_str == 'alexnet':
+        return models.alexnet(pretrained=True)
+    elif model_str == 'squeezenet':
+        return models.squeezenet1_0(pretrained=True)
+    elif model_str == 'vgg16':
+        return models.vgg16(pretrained=True)
+    elif model_str == 'densenet':
+        return models.densenet161(pretrained=True)
+    elif model_str == 'inception':
+        return models.inception_v3(pretrained=True)
+    elif model_str == 'googlenet':
+        return models.googlenet(pretrained=True)
+    elif model_str == 'shufflenet':
+        return models.shufflenet_v2_x1_0(pretrained=True)
+    elif model_str == 'mobilenet':
+        return models.mobilenet_v2(pretrained=True)
+    elif model_str == 'resnext50_32x4d':
+        return models.resnext50_32x4d(pretrained=True)
+    elif model_str == 'wide_resnet50_2':
+        return models.wide_resnet50_2(pretrained=True)
+    elif model_str == 'mnasnet':
+        return models.mnasnet1_0(pretrained=True)
     else:
         return NotImplementedError
